6fa145895241d83549a2e27eec0e96196b5a2e24
ATHIRA SIVADASAN <75289141+athirasivadasan1999@users.noreply.github.com>
2022-12-25 04:56:40 -0800

    Add files via upload

---
+++ model/employee.js
@@ -0,0 +1,14 @@
+let Mongoogse = require("mongoose");
+
+
+const employeeSchema = Mongoogse.Schema(
+    {
+        name: String,
+        position: String,
+        location: String,
+        salary: Number
+    }
+);
+
+let employeeModel = Mongoogse.model("employeeDetails", employeeSchema);
+module.exports = employeeModel;
